1fbb75aa64aa3a783a6c84c59b9504e40b84fdc4
czepedar <zeprm@hotmail.com>
2021-06-20 15:11:20 -0400

    Initial commit

---
+++ Example/MyFirstLibrary/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyFirstLibrary
+//
+//  Created by czepedar on 06/20/2021.
+//  Copyright (c) 2021 czepedar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
